78735d68510d94538222a319a69b07a2a6ed5db7
Anshupadhyay1419 <anshupadhyay1406@gmail.com>
2026-04-12 10:17:05 +0530

    feat: add Cloudinary remote pattern to next.config

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,13 @@ const nextConfig: NextConfig = {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
     minimumCacheTTL: 60 * 60 * 24 * 30,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+    ],
   },
   compress: true,
   poweredByHeader: false,
