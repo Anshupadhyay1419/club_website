daa300299d5e6939d372d416930937dc69cf9a50
Anshupadhyay1419 <anshupadhyay1406@gmail.com>
2026-08-13 20:08:42 +0530

    added image in gallery

--- data/gallery.ts
+++ data/gallery.ts
@@ -59,7 +59,7 @@ export const galleryImages: GalleryImage[] = [
   },
   {
     id: 'g8',
-    src: 'https://res.cloudinary.com/dfkbt26ga/image/upload/v1786631663/Screenshot_2026-08-13_200305_sno0vs.png',
+    src: 'https://res.cloudinary.com/dfkbt26ga/image/upload/v1786631663/Screenshot_2026-08-13_200305_sno0vs.jpg',
     alt: 'Hackathon 2026',
     caption: 'Deep sight Hackathon 24th April 2026',
     width: 1200,
